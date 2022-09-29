8ca27deb8e5da747af48643bf6632c8c2de9c115
Katya <katyalolka1994@gmail.com>
2022-09-29 19:24:29 +0300

    first commit

---
+++ a1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int cons = N;
+Output( cons, 1);
+void Output (int num, int constanta)
+{
+    if (num == 1 ) return;
+    if (num%2==0)
+    Console.Write($"{num}, ");
+    Output(num-1, constanta);
+}
---
+++ a2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введи N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = int.Parse(Console.ReadLine());
+int sum = 0;
+if (M > N)
+    for (int i = N; i <= M; i++)
+        {
+            sum = sum + i;
+            Console.Write($" {sum} ");
+        }
+else
+    for (int i = M; i <= N; i++)
+        {
+            sum = sum + i;
+            Console.Write($" {sum} ");
+        }
